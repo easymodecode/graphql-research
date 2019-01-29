8b68187ea5de4df6e2287c96386336e43b493b8c
Albert J Orlando <aorlando.git@gmail.com>
2019-01-28 21:30:09 -0500

    Add mutation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
 let links = [
   {
     id: 'link-0',
@@ -36,16 +23,29 @@ let links = [
   },
 ];
 
+let idCount = links.length;
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        descirption: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: 'src/schema.graphql',
   resolvers,
 });
 
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+server.start(() => console.log(`Listening on http://localhost:4000`));
